5800e3b361e346822e43ad632fbd9578e0d8e663
Minilin <a0966678326@gmail.com>
2022-10-20 11:02:16 +0800

    完成題目3

--- test3.php
+++ test3.php
@@ -10,12 +10,25 @@ foreach ($array as $key => $value) {
         array_push($even,$value);
     }
 }
+echo "奇數陣列:";
 print_r($odd);
+echo "<br>";
+echo "偶數陣列:";
 print_r($even);
-// 計算出 奇數總和 減 偶數總和
-$oddsum=array_sum($odd);
-$evensum=array_sum($even);
+echo "<br>";
 
+// 計算出 奇數總和 減 偶數總和
+// $oddsum=array_sum($odd);
+// $evensum=array_sum($even);
+$oddsum=0;
+$evensum=0;
+for ($i=0; $i <count($odd) ; $i++) { 
+    $oddsum=$oddsum+$odd[$i];
+}
+for ($j=0; $j <count($even) ; $j++) { 
+    $evensum=$evensum+$even[$j];
+}
 $anser=$oddsum-$evensum;
+echo "奇數總和減去偶數總和:";
 echo "$anser";
 ?>
