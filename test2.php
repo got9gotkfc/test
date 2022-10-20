480155a1ee9ad3a5bd85837c4266980bdc106fb4
Minilin <a0966678326@gmail.com>
2022-10-20 11:13:39 +0800

    完成題目2

--- test2.php
+++ test2.php
@@ -1,13 +1,14 @@
 <?php
 $string = "人易科計:上 機 測 驗 - 演 算 法";
 echo "1.".str_replace(":","：",$string);
+echo "<br>";
 
-$astring=str_replace(" ","",$string);
-$answer2=str_replace("-"," - ",$astring);
+$noblankstring=str_replace(" ","",$string);
+$answer2=str_replace("-"," - ",$noblankstring);
 echo "2.".$answer2;
+echo "<br>";
 
-
-$a=strpos($astring,":");
-$b=strpos($astring,"-");
-echo "3.".mb_substr($astring,$a,$b-$a);
+$a=strpos($string,":");
+$b=strpos($string,"-");
+echo "3.".substr($string,$a+1,$b-$a-1);
 ?>
