2f769f1172bc6c747c33aefdd0f9b0327c73f060
Minilin <a0966678326@gmail.com>
2022-10-20 11:49:17 +0800

    完成題目4

--- test4.php
+++ test4.php
@@ -2,7 +2,7 @@
 // 正序排序
 $array=[77,5,5,22,13,55,97,4,796,1,0,9];
 $arraylen=count($array);
-for ($i=0; $i <$arraylen-1 ; $i++) { 
+for ($i=0; $i <$arraylen ; $i++) { 
   for ($j=0; $j <$arraylen-1 ; $j++) { 
     if ($array[$j]>$array[$j+1]) {
         $a=$array[$j+1];
